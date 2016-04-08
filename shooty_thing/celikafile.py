d7c1fd483e281ef1179b658d4bb2deb9c58a5634
pendingchaos <pendingchaos02@gmail.com>
2016-04-08 12:05:36 +0100

    Added celikaproj

---
+++ shooty_thing/celikafile.py
@@ -0,0 +1,35 @@
+celika_dir = '../celika/'
+celika_cflags = '-pedantic -Wall -std=c11 -g'
+celika_ldflags = ''
+celika_cflags_native = ''
+celika_ldflags_native = ''
+celika_cflags_emscripten = ''
+celika_ldflags_emscripten = ''
+celika_runopt = ''
+celika_src = 'shooty_thing.c'
+celika_embedded_files = 'SpaceShooterRedux/PNG/playerShip3_green.png \
+SpaceShooterRedux/PNG/ufoRed.png \
+SpaceShooterRedux/PNG/ufoGreen.png \
+SpaceShooterRedux/PNG/ufoBlue.png \
+SpaceShooterRedux/PNG/ufoYellow.png \
+SpaceShooterRedux/PNG/Lasers/laserGreen10.png \
+SpaceShooterRedux/PNG/Lasers/laserRed16.png \
+SpaceShooterRedux/PNG/Power-ups/pill_yellow.png \
+SpaceShooterRedux/PNG/Power-ups/bolt_gold.png \
+SpaceShooterRedux/Backgrounds/black.png \
+SpaceShooterRedux/Backgrounds/blue.png \
+SpaceShooterRedux/Backgrounds/darkPurple.png \
+SpaceShooterRedux/Backgrounds/purple.png \
+shaders/passthough.glsl \
+SpaceShooterRedux/Bonus/sfx_laser1.ogg \
+SpaceShooterRedux/Bonus/sfx_laser2.ogg \
+SpaceShooterRedux/Bonus/sfx_lose.ogg \
+SpaceShooterRedux/Bonus/sfx_shieldUp.ogg \
+SpaceShooterRedux/Bonus/sfx_shieldDown.ogg \
+SpaceShooterRedux/Bonus/sfx_twoTone.ogg \
+SpaceShooterRedux/Bonus/sfx_zap.ogg \
+SpaceShooterRedux/PNG/Power-ups/shield_bronze.png \
+SpaceShooterRedux/PNG/Power-ups/shield_silver.png \
+SpaceShooterRedux/PNG/Power-ups/shield_gold.png \
+SpaceShooterRedux/PNG/Effects/shield1.png'
+celika_projname = 'shooty-thing'
